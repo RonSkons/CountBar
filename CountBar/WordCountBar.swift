3ace19e6129238763c630d0aae1608707e7e8799
Ron Friedman <ron852719@icloud.com>
2022-07-28 00:28:09 -0700

    Restored missing files

---
+++ CountBar/WordCountBar.swift
@@ -0,0 +1,47 @@
+//
+//  WordCountBar.swift
+//  CountBar
+//
+//  Created by Ron Friedman on 2022-07-27.
+//
+
+import AppKit
+
+class WordCountBar{
+    private var statusBar: NSStatusBar
+    private var statusItem: NSStatusItem
+    public var text: String
+    private var wordCount: Int = 0
+    private var charCount: Int = 0
+    private var showCharCount = true
+    
+    init(withText: String) {
+        //Initializes the status bar, counting the words in given string
+        statusBar = NSStatusBar.init()
+        statusItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+        text = withText
+        updateCount()
+        if let statusBarButton = statusItem.button{
+            statusBarButton.image = NSImage(systemSymbolName: "pencil.and.outline", accessibilityDescription: nil)
+            statusBarButton.imagePosition = NSControl.ImagePosition.imageLeft
+        }
+    }
+    
+    func updateCount(){
+        //Counts words and updates status bar display
+        countWords()
+        if let statusBarButton = statusItem.button{
+            let title = String(wordCount)+(showCharCount ? " / "+String(charCount):"")
+            statusBarButton.title = title
+        }
+    }
+    
+    func countWords(){
+        //Yucky one-liner; splits on spaces and newlines, removes empty "words", and counts
+        wordCount = text.components(separatedBy: .whitespacesAndNewlines).filter{!$0.isEmpty }.count
+        
+        //Removes newlines and counts remaining characters
+        charCount = text.components(separatedBy: .newlines).joined().count
+    }
+
+}
